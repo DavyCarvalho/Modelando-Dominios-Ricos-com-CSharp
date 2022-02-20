abfe682c119be7ae8b53424d3a740af8d3dcb080
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-20 18:17:50 -0300

    creating first entities

---
+++ PaymentContext/PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,30 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+    public abstract class Payment
+    {
+        public string Number { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime dateTime { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Owner { get; set; }
+        public string Document { get; set; }
+        public string BillingAddress { get; set; }
+        public string Email { get; set; }
+    }
+    public class BoletoPayment : Payment
+    {
+        public string BarCode { get; set; }
+        public string BoletoNumber { get; set; }
+    }
+    public class CreditCardPayment : Payment
+    {
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransactionNumber { get; set; }
+    }
+    public class PayPalPayment : Payment
+    {
+        public string TransactionCode { get; set; }
+    }
+}
