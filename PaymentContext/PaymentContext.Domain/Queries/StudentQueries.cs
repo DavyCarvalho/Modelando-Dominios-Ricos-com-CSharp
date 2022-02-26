2b00ca5377b12dac52d8c5db5d6882c88202ce6d
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-02-26 18:50:07 -0300

    creating querie for student and test for it

---
+++ PaymentContext/PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,13 @@
+﻿using PaymentContext.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace PaymentContext.Domain.Queries
+{
+    public class StudentQueries
+    {
+        public static Expression<Func<Student, bool>> GetStudentInfo(string document)
+        {
+            return x => x.Document.Number == document;
+        }
+    }
+}
